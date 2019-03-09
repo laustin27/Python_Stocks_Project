67d7f040a2bdfaaa7fa3ae5fdc043d6b9f8881f5
tla15 <timothy.l.austin@gmail.com>
2019-03-09 02:05:55 -0500

    Added command line argument functionality

--- tickers.py
+++ tickers.py
@@ -15,6 +15,7 @@ python3 tickers.py number of tickers ticker filename
 – The number n will be provided as a system argument to the module. There should be a main module
   that calls the save tickers function and passes the value of n to it. The value of n will be ≤ 150.
 """
+import sys
 import requests
 from pyquery import PyQuery
 from iex import Stock
@@ -28,14 +29,14 @@ def verifyTickers(symbol_list):
             symbol_list.remove(ticker)
 
 
-def saveTickers(n):
+def saveTickers(n, filename):
     """
     fetch the first n valid tickers from http://www.nasdaq.com/screening/companies-by-industry.aspx?exchange=NASDAQrender=download
     and writes the tickers in a file that is returned
     :return:
 
     """
-    if n > 150:
+    if int(n) > 150:
         raise Exception("You need to give me a number less than or equal to 150!")
 
     request = requests.get("https://www.nasdaq.com/screening/companies-by-industry.aspx?exchange=NASDAQ&pagesize=" + str(n))
@@ -47,7 +48,7 @@ def saveTickers(n):
     symbol_list = [symbol for symbol in symbols.text().split()]
     verifyTickers(symbol_list)
 
-    f = open("tickers.txt", "w")
+    f = open(filename, "w")
     for symbol in symbol_list:
         f.write(symbol + '\n')
 
@@ -55,5 +56,5 @@ def saveTickers(n):
 
 
 if __name__ == "__main__":
-    saveTickers(100)
+    saveTickers(sys.argv[1], sys.argv[2])
 
